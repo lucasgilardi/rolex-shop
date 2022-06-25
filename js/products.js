9c128afedf7837cf44ef9898a827972835047b50
Lucas Gilardi <lcsgilardi@gmail.com>
2022-06-25 12:23:31 -0300

    First commit

---
+++ js/products.js
@@ -0,0 +1,109 @@
+const products = [
+    {
+        "id": 0,
+        "img": "img/product1.png",
+        "title": "Spirit Rose", 
+        "price": 1500,
+        "instock" : 6
+    },
+    {
+        "id": 1,
+        "img": "img/product2.png",
+        "title": "Khaki Pilot", 
+        "price": 1350,
+        "instock" : 8
+    },
+    {
+        "id": 2,
+        "img": "img/product3.png",
+        "title": "Jubilee Black", 
+        "price": 870,
+        "instock" : 10
+    },
+    {
+        "id": 3,
+        "img": "img/product4.png",
+        "title": "Fosil Me3", 
+        "price": 650,
+        "instock" : 15
+    },
+    {
+        "id": 4,
+        "img": "img/product5.png",
+        "title": "Duchen", 
+        "price": 950,
+        "instock" : 9
+    },
+    {
+        "id": 6,
+        "img": "img/product6.png",
+        "title": "Dark Black", 
+        "price": 1950,
+        "instock" : 4
+    }
+]
+
+const featured = [
+    {
+        "id": 7,
+        "img": "img/featured1.png",
+        "title": "Jazzmaster", 
+        "price": 1050,
+        "instock" : 8
+    },
+    {
+        "id": 8,
+        "img": "img/featured2.png",
+        "title": "Ingersoll", 
+        "price": 250,
+        "instock" : 16
+    },
+    {
+        "id": 9,
+        "img": "img/featured3.png",
+        "title": "Rose Gold", 
+        "price": 890,
+        "instock" : 7
+    }
+]
+
+const newArrivals = [
+    {
+        "id": 10,
+        "img": "img/new1.png",
+        "title": "Longines Rose", 
+        "price": 980,
+        "instock" : 18
+    },
+    {
+        "id": 11,
+        "img": "img/new2.png",
+        "title": "Jazzmaster", 
+        "price": 1150,
+        "instock" : 9
+    },
+    {
+        "id": 12,
+        "img": "img/new3.png",
+        "title": "Dreyfuss Gold", 
+        "price": 750,
+        "instock" : 12
+    },
+    {
+        "id": 13,
+        "img": "img/new4.png",
+        "title": "Portuguese Rose", 
+        "price": 1590,
+        "instock" : 5
+    }
+]
+
+const home = [
+    {
+        "id": 14,
+        "img": "img/home.png",
+        "title": "B720 Gold", 
+        "price": 1534,
+        "instock" : 5
+    }
+]
